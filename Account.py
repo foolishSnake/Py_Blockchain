9b90b7d1b6482e750e0b1c3215f6f4055c1804a9
Phillip Hourigan <philosopher.phil.h@gmail.com>
2020-08-25 14:03:20 +0100

    Been working on the added accounts, needs more work.

--- Account.py
+++ Account.py
@@ -1,18 +1,11 @@
-import json
 
 
 class Account:
 
-    last_id = None
-    ACC_FILE = "AirgeadCryptoAccount.json"
-    balance = 0
-    transactions = []
-
-
-    def __init__(self, name, id):
+    def __init__(self, name, acc_id):
         self.name = name
-        self.id = id
-
+        self.acc_id = acc_id
+        self.balance = 0
 
     def increase_balance(self, amount):
         self.balance += amount
@@ -20,27 +13,6 @@ class Account:
     def decrease_balance(self, amount):
         self.balance -= amount
 
-    def add_account(self, name, start_balance):
-        account_dic = {"Account ID": self.last_id+1, "Name":name, "Balance": self.balance, "Transactions": []}
-        account_json = json.dumps(account_dic)
-        return account_dic
-
-    def save_account(self, acc_json):
-
-        return
-
-    def amend_balance(self, amount):
-        return
-
-
-    def add_transaction(self, from_acc, to_acc, amount, note):
-        trans_dic = {"From Account": from_acc, "To Account": to_acc, "Amount": amount, "Note": note}
-        trans_json = json.dumps(trans_dic)
-
-        return trans_json
-
-    def set_balance(self):
-        return
-
-
-
+    def account_dict(self):
+        acc_dict = {'Account ID': self.acc_id, 'Name': self.name, 'Balance': self.balance}
+        return acc_dict
--- AccountManager.py
+++ AccountManager.py
@@ -1,14 +1,57 @@
 import json
 from Account import Account
+import os.path
+import csv
+
 
 class AccountManager:
     accounts = []
-    last_id = None
+    last_id = 0
+    CSV_HEADER = ['Account ID', 'Name', 'Balance']
 
-    def add_count(self, name):
+    def __init__(self, account_file):
+        self.account_file = self.account_file
+        self.last_id = self.get_last_id()
+
+    def add_account(self, name):
+        new_acc = Account(self.last_id + 1, name)
+        self.accounts.append(new_acc)
+        self.last_id += 1
+        self.write_account(new_acc)
 
         return
 
-    def added_transaction(self, from_acc, to_acc, amount, note, block_hash ):
+    def get_last_id(self):
+        if os.path.isfile(self.account_file):
+            with open(self.account_file, 'r') as read:
+                csv_read = csv.DictReader(read, delimiter="|")
+                for line in csv_read:
+                    self.last_id = int(line['Account ID'])
+                return "Account ID retrieved"
 
-        return
+        else:
+            return "Can't find Blockchain file \"{}\"".format(self.account_file)
+
+    def get_account(self, acc_num):
+        if os.path.isfile(self.account_file):
+            with open(self.account_file, 'r') as read:
+                csv_read = csv.DictReader(read, delimiter="|")
+                for line in csv_read:
+                    if line['Account ID'] == acc_num:
+                        return line
+                    else:
+                        return "Data Not Found!"
+        else:
+            return "Can't find Accounts file \"{}\"".format(self.chain_file)
+
+
+    def write_account(self, account):
+        if not os.path.isfile(self.account_file):
+            with open(self.account_file, "w") as acc:
+                csv_write = csv.DictWriter(acc, fieldnames=self.CSV_HEADER, delimiter='|')
+                csv_write.writeheader()
+                csv_write.writerow(account.account_dict())
+        else:
+            with open(self.account_file, "a") as bc:
+                csv_write = csv.writer(bc, delimiter="|")
+                csv_write.writerow(self.account.account_dict())
