682d72bd1161d41bcf3fb14ef98e631e48bd6d74
Phillip Hourigan <philosopher.phil.h@gmail.com>
2020-09-02 15:01:55 +0100

    Still working on a method to increase the balance in AccountManager.py.

--- AccountManager.py
+++ AccountManager.py
@@ -7,6 +7,7 @@ class AccountManager:
     accounts = []
     last_id = 0
     CSV_HEADER = ['Account ID', 'Name', 'Balance']
+    ERROR_CODES = {1: "Record Not Found", 2: "File Not Found", 3: "Success"}
 
     def __init__(self, account_file):
         self.account_file = account_file
@@ -26,22 +27,25 @@ class AccountManager:
                 csv_read = csv.DictReader(read, delimiter="|")
                 for line in csv_read:
                     self.last_id = int(line['Account ID'])
-                return "Account ID retrieved"
+                return 3
 
         else:
-            return "Can't find Blockchain file \"{}\"".format(self.account_file)
+            return 2
 
     def get_account_file(self, acc_num):
         if os.path.isfile(self.account_file):
             with open(self.account_file, 'r') as read:
                 csv_read = csv.DictReader(read, delimiter="|")
                 for line in csv_read:
-                    if line['Account ID'] == acc_num:
-                        return line
+                    if int(line['Account ID']) == acc_num:
+                        new_acc = Account(int(line['Account ID']), line['Name'])
+                        new_acc.balance = int(line['Balance'])
+                        self.accounts.append(new_acc)
+                        return new_acc
                 else:
-                    return None
+                    return 1
         else:
-            return None
+            return 2
 
     def get_account_list(self, acc_num):
         if len(self.accounts) > 0:
@@ -49,9 +53,9 @@ class AccountManager:
                 if i.acc_id == acc_num:
                     return i
                 else:
-                    return None
+                    return 1
         else:
-            return None
+            return 1
 
     def write_account(self, account):
         if not os.path.isfile(self.account_file):
@@ -65,10 +69,13 @@ class AccountManager:
                 csv_write.writerow(account.account_dict())
 
     def increase_balance(self, acc_id, amount):
-        acc = self.get_account_file(acc_id)
-        if acc != "Data Not Found!" or acc != "Can't find Blockchain file \"{}\"".format(self.account_file):
+        acc = self.get_account(acc_id)
+        if acc == 1 or acc == 2:
+            return acc
+        else:
             new_acc = Account(acc['Account ID'], acc['Name'])
             acc_bal = int(acc['Balance'])
+            return 3
 
     def get_account(self, acc_id):
         """
@@ -80,10 +87,10 @@ class AccountManager:
         :return:
         """
         acc = self.get_account_list(acc_id)
-        if acc is None:
+        if acc is (1 or 2):
             acc_str = self.get_account_file(acc_id)
-            if acc_str is None:
-                return None
+            if acc_str is (1 or 2):
+                return acc_str
             else:
                 acc = Account(int(acc_str['Account ID']), acc_str['Name'])
                 acc.balance = int(acc_str['Balance'])
